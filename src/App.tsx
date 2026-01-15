1482651500fd5eef73a60be4c279fea95e1ec1af
alihusnain-dev <alihusnainmughal215@gmail.com>
2026-01-15 20:31:20 +0500

    add the gsap.from

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 function App() {
   useGSAP(() => {
-    gsap.to("#box1", {
+    gsap.to(["#box1", "#box2", "#box3"], {
       x: 100,
       duration: 2,
       repeat: -1,
@@ -11,20 +11,9 @@ function App() {
       ease: "power1.out",
       borderRadius: "50%",
     });
-    gsap.to("#box2", {
+    gsap.from(["#box1", "#box2", "#box3"], {
       x: 100,
       duration: 2,
-      delay: 1,
-      repeat: -1,
-      yoyo: true,
-      rotate: 360,
-      ease: "power1.out",
-      borderRadius: "50%",
-    });
-    gsap.to("#box3", {
-      x: 100,
-      duration: 2,
-      delay: 2,
       repeat: -1,
       yoyo: true,
       rotate: 360,
@@ -35,11 +24,21 @@ function App() {
   return (
     <>
       <div className="bg-black h-screen w-screen overflow-hidden">
-        <h1 className="text-4xl text-red-600">Hello World</h1>
         <div className="w-screen mt-12">
-          <div className="h-52 w-52 bg-red-600 mb-8" id="box1"></div>
-          <div className="h-52 w-52 bg-blue-600 mb-8" id="box2"></div>
-          <div className="h-52 w-52 bg-green-600 mb-8" id="box3"></div>
+          <h1 className="text-4xl text-red-600">Gsap.to</h1>
+          <div className="w-screen mt-12">
+            <div className="h-52 w-52 bg-red-600 mb-8" id="box1"></div>
+            <div className="h-52 w-52 bg-blue-600 mb-8" id="box2"></div>
+            <div className="h-52 w-52 bg-green-600 mb-8" id="box3"></div>
+          </div>
+        </div>
+        <div className="w-screen mt-12">
+          <h1 className="text-4xl text-red-600">Gsap.from</h1>
+          <div className="w-screen mt-12">
+            <div className="h-52 w-52 bg-red-600 mb-8" id="box1"></div>
+            <div className="h-52 w-52 bg-blue-600 mb-8" id="box2"></div>
+            <div className="h-52 w-52 bg-green-600 mb-8" id="box3"></div>
+          </div>
         </div>
       </div>
     </>
